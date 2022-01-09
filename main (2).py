29707079fff8d35a4b837e9ebf8f0cb4d818468c
Luci <93726807+Brane105@users.noreply.github.com>
2022-01-09 03:23:30 -0800

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,5 @@
+#generate a code to display month and year using calender 
+import calendar
+year = int(input("enter the year: ")) #month 
+month = int(input("enter the month: ")) #year
+print(calendar.month(year,month)) #output
